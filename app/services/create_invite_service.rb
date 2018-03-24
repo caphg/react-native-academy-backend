00a5300112a81aa698868bd3c5e08133c9aeef23
Hrvoje G <hrvoje.grdic1@gmail.com>
2018-03-24 23:33:28 +0100

    case for invite sent

--- app/services/create_invite_service.rb
+++ app/services/create_invite_service.rb
@@ -6,6 +6,7 @@ class CreateInviteService
   def call(params)
     second_user = second_user(params)
     raise Invitation::TodoCannotBeShared.new('Todo already shared') if !todo_sharable?(second_user, params[:todo_id])
+    raise Invitation::TodoCannotBeShared.new('Invitation already sent') if todo_sent?(second_user, params[:todo_id])
     raise Invitation::TodoCannotBeShared.new(
       'You are owner of this todo, sharing it with others is more fun ;)'
     ) if sharing_with_myself(second_user)
@@ -26,5 +27,11 @@ class CreateInviteService
 
   def sharing_with_myself(second_user)
     @first_user.id == second_user.id
+  end
+
+  def todo_sent?(second_user, todo_id)
+    Invitation
+      .where(first_user_id: @first_user.id, second_user_id: second_user.id, todo_id: todo_id)
+      .count > 0
   end
 end
